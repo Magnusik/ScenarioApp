179ce14008aa973abfd16e890ae68dd31c5e1d44
Magnus Kolbeinsen <magnuskolbeinsen@hotmail.com>
2022-07-27 16:11:18 +0200

    fixed code, added threading for sounds,fixed randomize function for ledOn

--- simApp.py
+++ simApp.py
@@ -12,6 +12,7 @@ from kivy.uix.textinput import TextInput
 from random import randint
 import numpy as np
 from functools import partial
+import threading
 
 
 class MedicalScenario(App):
@@ -23,12 +24,12 @@ class MedicalScenario(App):
         
         self.connectArduino
         self.board = Arduino("COM8")
-        self.states = ['crying','screaming','whining','dead'] 
+        self.states = ['crying','screaming','whining'] 
         self.scenario = "neutral"
         self.guess ="nothing"
         self.connectguard = True
         # image widget
-        self.window.add_widget(Image(source="bacon.png"))
+        self.window.add_widget(Image(source="Laerdal_logo.png"))
 
         #label widget
         self.greeting = Label(
@@ -37,16 +38,6 @@ class MedicalScenario(App):
             color='#00FFCE'
             )
         self.window.add_widget(self.greeting)
-
-        #text input widget
-        # self.user = TextInput(
-        #     multiline = False,
-        #     padding_y = (20,20),
-        #     size_hint = (1,0.5)
-        #     )
-        # self.window.add_widget(self.user)
-        
-        #button widgets
         
         self.buttonScenario = Button(
             text="Start Scenario",
@@ -83,52 +74,36 @@ class MedicalScenario(App):
             )
         self.buttonWhining.bind(on_press=partial(self.changeState, bvalue ='whining'))
         self.window.add_widget(self.buttonWhining)
-        
-        self.buttonDead = Button(
-            text="Dead",
-            size_hint = (1,0.5),
-            bold = True,
-            background_color = "#00FFCE"
-            )
-        self.buttonDead.bind(on_press=partial(self.changeState, bvalue ='dead'))
-        self.window.add_widget(self.buttonDead)
 
         return self.window
     
-    def startLedShow(self,instance):
-        board = Arduino("COM8")
-        iterator = util.Iterator(board)
-        iterator.start()
-        valve_pin8 = board.get_pin("d:9:p")
-        while True:
-            board.digital[9].write(1)
-            time.sleep(4)
-            board.digital[9].write(0)
-            time.sleep(1)
-    
     def ledOn(self,instance):
-        self.scenario = np.random.choice(self.states,size=1)
+        new_scenario = self.states.copy()
+        if self.scenario == 'neutral':
+            self.scenario = np.random.choice(self.states,size=1)
+        else:
+            new_scenario.remove(self.scenario)
+            self.scenario = np.random.choice(new_scenario,size=1)
+        
         print(self.scenario)
         if self.scenario == "crying":
             self.board.digital[8].write(1)
             self.board.digital[9].write(0)
             self.board.digital[10].write(0)
-            playsound('C:/Users/NOMKO3/Random_scripts/PythonApp/ScenarioApp/tunes/crying.mp3')
+            cry_th = threading.Thread(target=self.sound,args=('crying',))
+            cry_th.start()
         if self.scenario == "screaming":
             self.board.digital[9].write(1)
             self.board.digital[10].write(0)     
             self.board.digital[8].write(0)
-            playsound('C:/Users/NOMKO3/Random_scripts/PythonApp/ScenarioApp/tunes/screaming.mp3')
+            scream_th = threading.Thread(target=self.sound,args=('screaming',))
+            scream_th.start()
         if self.scenario == "whining":
             self.board.digital[8].write(0)
             self.board.digital[9].write(0)
             self.board.digital[10].write(1)
-            playsound('C:/Users/NOMKO3/Random_scripts/PythonApp/ScenarioApp/tunes/whining.mp3')
-    
-    def ledClear(self,instance):
-        self.board.digital[8].write(0)
-        self.board.digital[9].write(0)
-        self.board.digital[10].write(0)
+            whining_th = threading.Thread(target=self.sound,args=('whining',))
+            whining_th.start()
     
     def connectArduino():
             board = Arduino("COM8")
@@ -141,10 +116,8 @@ class MedicalScenario(App):
     def callback(self,instance):
         self.greeting.text = "Hello " + self.user.text + "!",
         print(self.scenario)
+
     def changeState(self,instance,bvalue):
-        if bvalue == "dead":
-            self.guess="dead"
-            print(self.guess)
         if bvalue == "screaming":
             self.guess="screaming"
             print(self.guess)
@@ -160,8 +133,6 @@ class MedicalScenario(App):
             self.correctCallBack()
         else:
             self.wrongCallBack()
-
-
 
     def  answerGuess(self):
         guess = self.guess
@@ -179,180 +150,14 @@ class MedicalScenario(App):
     def wrongCallBack(self):
         self.greeting.color='#FF0000'
         self.greeting.text = "Wrong!"
+    def sound(self,sound):
+        if sound == 'crying':
+            playsound('C:/Users/NOMKO3/Random_scripts/PythonApp/ScenarioApp/tunes/crying.mp3')
+        if sound == 'whining':
+            playsound('C:/Users/NOMKO3/Random_scripts/PythonApp/ScenarioApp/tunes/whining.mp3')
+        if sound == 'screaming':
+            playsound('C:/Users/NOMKO3/Random_scripts/PythonApp/ScenarioApp/tunes/screaming.mp3')
+        else: return 0
+
 if __name__ == "__main__":
-    MedicalScenario().run()
-
-## unbind all in scenario button
-# import time
-# from pyfirmata import Arduino, util
-
-# board = Arduino("COM10")
-
-# iterator = util.Iterator(board)
-# iterator.start()
-
-# valve_pin8 = board.get_pin("d:9:p")
-
-# while True:
-#     board.digital[9].write(1)
-#     #valve_pin8.write(1)
-#     time.sleep(4)
-#     board.digital[9].write(0)
-#     #valve_pin8.write(0)
-#     time.sleep(1)
-
-"""
-Based on the 1s-pulse.py. See python-EK-M-015-main/1s-pulse.py for explaination of serial functions.
-To use this with an Arduino Uno, you must first upload the StandardFirmata sketch from Arduino.
-This can be found in File -> Examples -> Firmata.
-"""
-
-# import threading
-# import time
-# import serial
-# import time
-# from playsound import playsound
-# from pyfirmata import Arduino, util
-
-# rapid_breath = True
-
-# board = Arduino("COM9")
-
-# iterator = util.Iterator(board)
-# iterator.start()
-
-# valve_pin7 = board.get_pin("d:7:o")
-# valve_pin8 = board.get_pin("d:8:o")
-# motor_pin2 = board.get_pin("d:2:o")
-# motor_pin3 = board.get_pin("d:3:o")
-
-# ventus_port = serial.Serial(port="COM8",
-#                             baudrate=115200,
-#                             bytesize=8,
-#                             timeout=2,
-#                             stopbits=serial.STOPBITS_ONE)
-# ventus_port.write(b"#W2,0\n")
-# ventus_port.write(b"#W0,0\n")
-# ventus_port.write(b"#W10,0\n")
-# ventus_port.write(b"#W11,0\n")
-
-# if rapid_breath:
-#     ventus_port.write(b"#W23,1000\n")
-# else:
-#     ventus_port.write(b"#W23,100\n")
-
-# def digitalWrite(pin, value):
-#     if pin == 8:
-#         valve_pin8.write(value)
-#     elif pin == 7:
-#         valve_pin7.write(value)
-#     elif pin == 3:
-#         motor_pin3.write(value)
-#     elif pin == 2:
-#         motor_pin2.write(value)
-
-# def write_pump(value):
-#     if value == 1:
-#         ventus_port.write(b"#W0,1\n")
-#     else:
-#         ventus_port.write(b"#W0,0\n")
-
-# def play_sound(filepath):
-#     playsound(filepath)
-
-# while True:
-#     if rapid_breath:
-#         breath_path = "C:\\Users\\NOLEL1\\Documents\\LaerdalSkinRetractions\\breathing_sounds\\08INNpust.mp3"
-#         #valve_8_inhale = threading.Thread(target=digitalWrite,args=(8,1,))
-#         valve_7_inhale = threading.Thread(target=digitalWrite,args=(7,1,))
-#         #motor_3_inhale = threading.Thread(target=digitalWrite,args=(3,1))
-#         #motor_2_inhale = threading.Thread(target=digitalWrite,args=(2,0))
-#         pump_inhale = threading.Thread(target=write_pump,args=(1,))
-#         sound_inhale = threading.Thread(target=play_sound,args=(breath_path,))
-
-#         #valve_8_inhale.start()
-#         valve_7_inhale.start()
-#         #motor_3_inhale.start()
-#         #motor_2_inhale.start()
-#         pump_inhale.start()
-#         sound_inhale.start()
-
-#         time.sleep(1.1)
-#         #valve_8_inhale.join()
-#         valve_7_inhale.join()
-#         #motor_3_inhale.join()
-#         #motor_2_inhale.join()
-#         pump_inhale.join()
-#         sound_inhale.join()
-
-#         breath_path = "C:\\Users\\NOLEL1\\Documents\\LaerdalSkinRetractions\\breathing_sounds\\08UTpust.mp3"
-        
-#         valve_8_exhale = threading.Thread(target=digitalWrite,args=(8,0,))
-#         valve_7_exhale = threading.Thread(target=digitalWrite,args=(7,0,))
-#         #motor_3_exhale = threading.Thread(target=digitalWrite,args=(3,0))
-#         #motor_2_exhale = threading.Thread(target=digitalWrite,args=(2,1))
-#         pump_exhale = threading.Thread(target=write_pump,args=(0,))
-#         sound_exhale = threading.Thread(target=play_sound,args=(breath_path,))
-
-#         #valve_8_exhale.start()
-#         valve_7_exhale.start()
-#         #motor_3_exhale.start()
-#         #motor_2_exhale.start()
-#         pump_exhale.start()
-#         sound_exhale.start()
-
-#         time.sleep(0.9)
-#         #valve_8_exhale.join()
-#         valve_7_exhale.join()
-#         #motor_3_exhale.join()
-#         #motor_2_exhale.join()
-#         pump_exhale.join()
-#         sound_exhale.join()
-
-#     else:
-#         breath_path = "C:\\Users\\NOLEL1\\Documents\\LaerdalSkinRetractions\\breathing_sounds\\12INNpust.mp3"
-#         #valve_8_inhale = threading.Thread(target=digitalWrite,args=(8,1,))
-#         valve_7_inhale = threading.Thread(target=digitalWrite,args=(7,1,))
-#         #motor_3_inhale = threading.Thread(target=digitalWrite,args=(3,1))
-#         #motor_2_inhale = threading.Thread(target=digitalWrite,args=(2,0))
-#         pump_inhale = threading.Thread(target=write_pump,args=(1,))
-#         sound_inhale = threading.Thread(target=play_sound,args=(breath_path,))
-
-#         #valve_8_inhale.start()
-#         valve_7_inhale.start()
-#         #motor_3_inhale.start()
-#         #motor_2_inhale.start()
-#         pump_inhale.start()
-#         sound_inhale.start()
-
-#         time.sleep(2)
-#         #valve_8_inhale.join()
-#         valve_7_inhale.join()
-#         #motor_3_inhale.join()
-#         #motor_2_inhale.join()
-#         pump_inhale.join()
-#         sound_inhale.join()
-
-#         breath_path = "C:\\Users\\NOLEL1\\Documents\\LaerdalSkinRetractions\\breathing_sounds\\12Utpust.mp3"
-        
-#         #valve_8_exhale = threading.Thread(target=digitalWrite,args=(8,0,))
-#         valve_7_exhale = threading.Thread(target=digitalWrite,args=(7,0,))
-#         #motor_3_exhale = threading.Thread(target=digitalWrite,args=(3,0))
-#         #motor_2_exhale = threading.Thread(target=digitalWrite,args=(2,1))
-#         pump_exhale = threading.Thread(target=write_pump,args=(0,))
-#         sound_exhale = threading.Thread(target=play_sound,args=(breath_path,))
-
-#         #valve_8_exhale.start()
-#         valve_7_exhale.start()
-#         #motor_3_exhale.start()
-#         #motor_2_exhale.start()
-#         pump_exhale.start()
-#         sound_exhale.start()
-
-#         time.sleep(2)
-#         #valve_8_exhale.join()
-#         valve_7_exhale.join()
-#         #motor_3_exhale.join()
-#         #motor_2_exhale.join()
-#         pump_exhale.join()
-#         sound_exhale.join()
+    MedicalScenario().run()   
